9d82a942c85f67cb35ec1274912a177c31639682
reenadgh <miss_roreee@hotmail.com>
2022-12-21 13:49:14 +0300

    initialization

---
+++ UserLogin/UserLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLoginApp.swift
+//  UserLogin
+//
+//  Created by Reenad gh on 27/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct UserLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
